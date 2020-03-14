f69b621d6dce0d4b914b7156cc4d2c1ae65245af
Juveria Manzar <juveriadot@gmail.com>
2020-03-15 01:45:37 +0530

    checks for changes in js file

---
+++ nodemontest.js
@@ -0,0 +1,9 @@
+/**
+ * @author Juveria-Manzar
+ * @email juveriamanzar29@gmail.com
+ * @create date 2020-03-15 01:43:17
+ * @modify date 2020-03-15 01:43:17
+ * @desc nodemon checks for changes in node files
+ */
+
+console.log('file changed again')
